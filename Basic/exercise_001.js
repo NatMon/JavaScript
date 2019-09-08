984a362ed7b331681a382d874452d3473ca9efc6
Natalia Monsalve <natalia.monsalve.m@gmail.com>
2019-09-08 10:22:30 -0400

    Update exercise_001.js

--- Basic/exercise_001.js
+++ Basic/exercise_001.js
@@ -1,5 +1,4 @@
-/*
-1. Write a JavaScript program to display the current day and time in the following format:
+/* 1. Write a JavaScript program to display the current day and time in the following format:
 
 Sample Output : 
 

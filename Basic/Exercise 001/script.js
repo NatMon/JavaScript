7fe86b40d60ed1c2a060a0c2cb969257566ecdaf
Natalia Monsalve <natalia.monsalve.m@gmail.com>
2020-08-11 17:46:10 -0400

    Update script.js

--- Basic/Exercise 001/script.js
+++ Basic/Exercise 001/script.js
@@ -1,1 +1,31 @@
+var today = new Date();
+var daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+var day = daysOfWeek[today.getDay()];
 
+var hourAmPm;
+
+function ampm() {
+  var hour = today.getHours();
+  if (hour < 12) {
+    if (hour == 0) {
+      hourAmPm = 12 + " AM";
+    }
+    else {
+      hourAmPm = hour + " AM";
+    }
+  }
+  else {
+    if (hour == 12) {
+      hourAmPm = 12 + " PM";
+    }
+    else {
+      hour = hour % 12;
+      hourAmPm = hour + " PM";
+    }
+  }
+}
+
+ampm();
+
+console.log("Today is : " + day);
+console.log("Current time is : " + hourAmPm + " : " + today.getMinutes() + " : " + today.getSeconds());
